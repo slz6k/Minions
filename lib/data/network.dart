eb49fbaaa880547f43629e8701da79b9a380eadd
민경 <tjalsrud0101@gmail.com>
2023-10-21 00:17:56 +0900

    feat: fetch air pollution data (#20)

--- lib/data/network.dart
+++ lib/data/network.dart
@@ -3,10 +3,20 @@ import 'dart:convert';
 
 class Network {
   final String url;
-  Network(this.url);
+  final String pollutionUrl;
+  Network(this.url, this.pollutionUrl);
 
   Future<dynamic> getJsonData() async {
     http.Response response = await http.get(Uri.parse(url));
+    if (response.statusCode == 200) {
+      String jsonData = response.body;
+      var parsingData = jsonDecode(jsonData);
+      return parsingData;
+    }
+  }
+
+  Future<dynamic> getAirData() async {
+    http.Response response = await http.get(Uri.parse(pollutionUrl));
     if (response.statusCode == 200) {
       String jsonData = response.body;
       var parsingData = jsonDecode(jsonData);
